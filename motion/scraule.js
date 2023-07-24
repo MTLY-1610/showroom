459ef14eb2948e4e91be5b3fc6c9d603b9b8c0ad
MTLY-1610 <larrouy.m@gmail.com>
2023-07-24 15:45:35 +0200

    scroll fail #1 but :)

--- motion/scraule.js
+++ motion/scraule.js
@@ -1,12 +1,12 @@
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+});
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+  lenis.raf(time);
+  ScrollTrigger.update();
+  requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
