52e38285e3d92d1552067608b9e9368221bba5b4
MTLY-1610 <larrouy.m@gmail.com>
2023-07-28 17:11:10 +0200

    landing 1.0

--- motion/main_nav.js
+++ motion/main_nav.js
@@ -29,12 +29,9 @@ requestAnimationFrame(raf);
 const tl = gsap.timeline();
 
 tl.fromTo(".one", {opacity: 0}, {opacity: 1, duration: 0.7}, "0.9")
-tl.to(".one", { duration: 2, y: -100}, "1.2");
 tl.fromTo(".two", {opacity: 0}, {opacity: 1, duration: 0.7}, "1")
-tl.to(".two", { duration: 2, y: -100}, "0.9");
 tl.fromTo(".three", {opacity: 0}, {opacity: 1, duration: 0.7}, "1.4")
-tl.to(".three", { duration: 2, y: -100}, "0.9");
-tl.fromTo(".menu", {opacity: 0}, {opacity: 1, duration: 0.7}, "3");
+tl.fromTo(".menu", {opacity: 0}, {opacity: 1, duration: 0.7}, "2.2s");
 
 
 
